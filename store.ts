0fe18f2e32d7442efba204d01de8ff426184d353
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-27 15:57:28 -0600

    instalacion de axios

--- store.ts
+++ store.ts
@@ -1,7 +1,12 @@
 import { create } from "zustand";
 
+async function getCryptos(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+}
+
 export const useCryptoStore = create (() => ({
     fetchCryptos: () => {
         console.log('desde fetch cryptos')
     }
 }))
+
