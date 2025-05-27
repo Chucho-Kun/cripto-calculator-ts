8a179a19a07b277a29f3afaa07f9d3fabee8d5ef
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-27 09:34:41 -0600

    instalacion de Zod

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,10 @@
+import { currencies } from "../data"
+
 export default function CriptoSearchForm() {
   return (
-    <form>
+    <>
+    
+    <form className="form">
         <div>
             <label htmlFor="currency">Moneda:</label>
             <select 
@@ -8,8 +12,25 @@ export default function CriptoSearchForm() {
                 id="currency"
             >
                 <option value="">--- Seleccione</option>
+               
+                {currencies.map( currency => (
+                    <option key={currency.code} value={currency.code}>{currency.name}</option>
+                ))}
             </select>
         </div>
+
+        <div>
+            <label htmlFor="criptocurrency">Cripto Moneda:</label>
+            <select 
+                name="criptocurrency" 
+                id="criptocurrency"
+            >
+                <option value="">--- Seleccione</option>
+            </select>
+        </div>
+
+        <input type="submit" value='Cotizar' />
     </form>
+    </>
   )
 }
