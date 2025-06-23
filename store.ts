42ec3a65cc9d1e7f41a204bf3848925c7471355f
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-23 10:59:13 -0600

    vinculando los types con zod

--- store.ts
+++ store.ts
@@ -1,12 +1,29 @@
 import { create } from "zustand";
+import { CryptoCurrenciesResponseSchema ,  } from './src/schema/crypto-schema';
+import axios from "axios";
+import type { Cryptocurrency } from "./src/types";
+
+
+type CryptoStore = {
+    cryptoCurrency: Cryptocurrency[],
+    fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos(){
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const { data : { Data } } = await axios(url)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    if( result.success ){
+        return result.data
+    }
 }
 
-export const useCryptoStore = create (() => ({
-    fetchCryptos: () => {
-        console.log('desde fetch cryptos')
+export const useCryptoStore = create <CryptoStore> (( set ) => ({
+    cryptoCurrency: [],
+    fetchCryptos: async () => {
+        const cryptoCurrency = await getCryptos()
+        console.log( {cryptoCurrency} )
+        set ( { cryptoCurrency } )
     }
 }))
 
